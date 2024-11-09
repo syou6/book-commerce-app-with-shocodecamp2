65df9fb8359784489f894f36adacac99d80f8380
sho kawamoto <k.sho626626@gmail.com>
2024-11-09 10:32:11 +0900

    done

--- app/api/purchases/[userId]/route.ts
+++ app/api/purchases/[userId]/route.ts
@@ -4,16 +4,16 @@ import { NextResponse } from "next/server";
 //購入履歴検索API
 export async function GET(
     request: Request,
-    { params }: { params: { userId: string } }
+    context: { params: { userId: string } }
 ) {
-    const userId = params.userId;
+    const userId = context.params.userId;
 
     try {
         const purchases = await prisma.purchase.findMany({
             where: { userId: userId },
         });
         return NextResponse.json(purchases);
-    } catch {
+    } catch (err) {
         return NextResponse.json({ error: "Error fetching purchases" });
     }
 }
