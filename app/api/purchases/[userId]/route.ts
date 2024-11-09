2dff0e7e0ec9cf034983e8fa6183dbfbc0ca5b6a
sho kawamoto <k.sho626626@gmail.com>
2024-11-09 10:30:09 +0900

    done

--- app/api/purchases/[userId]/route.ts
+++ app/api/purchases/[userId]/route.ts
@@ -13,7 +13,7 @@ export async function GET(
             where: { userId: userId },
         });
         return NextResponse.json(purchases);
-    } catch (_) {
+    } catch {
         return NextResponse.json({ error: "Error fetching purchases" });
     }
 }
