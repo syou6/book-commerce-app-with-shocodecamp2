984842682f18062f4428284e836d5087709b038f
sho kawamoto <k.sho626626@gmail.com>
2024-11-09 10:37:05 +0900

    done

--- app/api/purchases/[userId]/route.ts
+++ app/api/purchases/[userId]/route.ts
@@ -1,19 +1,20 @@
 import prisma from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 
-//購入履歴検索API
+//https://nextjs.org/docs/app/building-your-application/routing/route-handlers#dynamic-route-segments
 export async function GET(
-    request: Request,
-    context: { params: { userId: string } }
+  { params }: { params: { userId: string } }
 ) {
-    const userId = context.params.userId;
+  const userId = params.userId;
 
-    try {
-        const purchases = await prisma.purchase.findMany({
-            where: { userId: userId },
-        });
-        return NextResponse.json(purchases);
-    } catch {
-        return NextResponse.json({ error: "Error fetching purchases" });
-    }
+  try {
+    const purchase = await prisma.purchase.findMany({
+      where: { userId: userId },
+    });
+    console.log(purchase);
+
+    return NextResponse.json(purchase);
+  } catch (err) {
+    return NextResponse.json(err);
+  }
 }
