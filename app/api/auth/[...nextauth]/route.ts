385c9c63e99355a8cd7bf60fd419b783d2b747cf
sho kawamoto <k.sho626626@gmail.com>
2024-11-10 19:41:17 +0900

    done

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,12 @@
-import { nextAuthOptions } from "@/app/lib/next-auth/opitions";
-import NextAuth from "next-auth/next";
+import { NextApiRequest, NextApiResponse } from 'next';
+import NextAuth from 'next-auth';
+import { nextAuthOptions } from '../../../lib/next-auth/opitions';
 
-const handler = NextAuth(nextAuthOptions);
-
-export {handler as GET, handler as POST };
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    return await NextAuth(req, res, nextAuthOptions);
+  } catch (error) {
+    console.error('NextAuth Error:', error);
+    res.status(500).json({ error: '認証エラーが発生しました' });
+  }
+}
