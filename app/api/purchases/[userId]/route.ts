f309b64e94cefdb1cfb9fc76f246f68bde86454a
sho kawamoto <k.sho626626@gmail.com>
2024-11-18 17:54:00 +0900

    done

--- app/api/purchases/[userId]/route.ts
+++ app/api/purchases/[userId]/route.ts
@@ -1,32 +1,28 @@
 import prisma from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 //https://nextjs.org/docs/app/building-your-application/routing/route-handlers#dynamic-route-segments
-export async function GET(
-  request: Request,
-  { params }: { params: { userId: string } }
-) {
-  const { userId } = params;
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const userId = searchParams.get('userId');
 
   if (!userId) {
     return NextResponse.json({ error: "User ID is required" }, { status: 400 });
   }
 
   try {
-    const purchases = await prisma.purchase.findMany({
+    const purchase = await prisma.purchase.findMany({
       where: { userId: userId },
     });
-    console.log("取得した購入データ:", purchases);
+    console.log(purchase);
 
-    return NextResponse.json(purchases);
+    return NextResponse.json(purchase);
   } catch (err) {
-    console.error("Error fetching purchases:", err);
-    return NextResponse.json(
-      { 
-        error: "An error occurred", 
-        details: (err as Error).message 
-      }, 
-      { status: 500 }
-    );
+    console.error(err);
+    return NextResponse.json({ 
+      error: "An error occurred", 
+      details: (err as Error).message
+    }, { status: 500 });
   }
 }
