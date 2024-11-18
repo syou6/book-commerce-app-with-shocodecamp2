5ee5bc0d5f63f9e4cc62c49c6f3b29b5e4da0d02
sho kawamoto <k.sho626626@gmail.com>
2024-11-18 15:07:25 +0900

    done

--- app/lib/next-auth/options.ts
+++ app/lib/next-auth/options.ts
@@ -8,36 +8,18 @@ export const nextAuthOptions: NextAuthOptions = {
   debug: true,
   providers: [
     GitHubProvider({
-      // profile(profile) {
-      //   console.log("Profile Github: ", profile);
-      //   let userRole = "GitHub User";
-      //   if (profile?.email === "shincode0712@gmail.com") {
-      //     userRole = "admin";
-      //   }
-
-      //   return {
-      //     id: profile.id.toString(), // 'id' を string 型に変換
-      //     name: profile.name,
-      //     email: profile.email,
-      //     image: profile.avatar_url,
-      //     role: userRole, // 追加されるカスタムプロパティ
-      //     // 必要に応じて他のプロパティを追加
-      //   };
-      // },
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
   ],
   adapter: PrismaAdapter(prisma),
   callbacks: {
-    session: ({ session, user }) => {
-      return {
-        ...session,
-        user: {
-          ...session.user,
-          id: user.id,
-        },
-      };
+    async session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+        session.user.image = user.image || null;
+      }
+      return session;
     },
   },
   secret: process.env.NEXTAUTH_SECRET,
