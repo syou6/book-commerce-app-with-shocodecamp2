86a2953e96e52c402b24846a9e8df949a5525a39
sho kawamoto <k.sho626626@gmail.com>
2024-11-19 17:46:34 +0900

    done

--- app/lib/microcms/client.ts
+++ app/lib/microcms/client.ts
@@ -10,7 +10,9 @@ export const getAllBooks = async () => {
     const allBooks = await client.getList<BookType>({
         endpoint: "e-book",
         customRequestInit: {
-          cache: "no-store",
+          next: {
+            revalidate: 3600
+          }
         },
     });
     return allBooks;
@@ -20,6 +22,9 @@ export const getDetailBook = async (contentId: string) => {
   const detailBook = await client.getListDetail<BookType>({
     endpoint: "e-book",
     contentId,
+    customRequestInit: {
+      cache: "no-store",
+    },
   });
   return detailBook;
 };
